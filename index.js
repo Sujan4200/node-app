79c736678dd84318787b33021f0dd1157f605124
Sujan Rokaya <92717662+Sujan4200@users.noreply.github.com>
2022-04-04 14:58:26 +0545

    Update index.js
    changed the name of app to app

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
-var app = express();
+var appp= express();
 
-app.get('/', function (req, res) {
+appp.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
